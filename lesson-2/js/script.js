061cf2b7c01b43094b82f016b83c4aac2658739c
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-08-12 21:44:51 +0300

    Update script.js

--- lesson-2/js/script.js
+++ lesson-2/js/script.js
@@ -26,6 +26,35 @@ for (let i = 0; i < 3; i++) {
 }//конец цикла
 
 
+// вторая версия цикла
+// let i = 0;
+// while (i < 3) {
+// 	let shopProd = prompt('Какой тип товаров будем продавать?');
+
+// 	if ((typeof(shopProd)) === 'string' && (typeof(shopProd)) != null && shopProd != '' && shopProd.length < 50 ) {
+// 		console.log('Все верно!');
+// 		mainList.shopGoods[i] = shopProd;
+// 	} else {
+// 		i = i-1;
+// 	}
+// 	i++;
+// }
+
+// третья версия цикла
+// let i = 0;
+// do {
+	
+// 	let shopProd = prompt('Какой тип товаров будем продавать?');
+
+// 	if ((typeof(shopProd)) === 'string' && (typeof(shopProd)) != null && shopProd != '' && shopProd.length < 50 ) {
+// 		console.log('Все верно!');
+// 		mainList.shopGoods[i] = shopProd;
+// 	} else {
+// 		i = i-1;
+// 	}
+// 	i++;
+// } while (i < 3);
+
 console.log (' бюджет на 1 день: ' + mainList.moneyMainList/30 );
 console.log (mainList.shopGoods);
 
