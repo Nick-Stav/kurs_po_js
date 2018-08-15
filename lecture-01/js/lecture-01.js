89c7b03491c1515385be73e0cb331a96356961de
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-08-15 23:18:16 +0300

    Update lecture-01.js

--- lecture-01/js/lecture-01.js
+++ lecture-01/js/lecture-01.js
@@ -5,5 +5,5 @@ for (var i = 2; i <= 100; i++) {
     if (i % a == 0) continue nextPrime;
   }
 
-  console.log( i ); // простые числа
+  console.log( i + " - Делители этого числа: 1 и " + i); // простые числа
 }
