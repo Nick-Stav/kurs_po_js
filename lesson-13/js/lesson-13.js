476024391f66af19787588164988daae17319620
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-01 12:47:42 +0300

    изменения на 01.09.2018

--- lesson-13/js/lesson-13.js
+++ lesson-13/js/lesson-13.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){ //конструкция, при которой код начинает работать только после загрузки страницы
 
+	//модалка на кнопку "выбрать тур"
 	$('.main_btna').click(function(){
+
+		$('.overlay').fadeToggle('slow');
+
+		$('.modal').slideToggle('slow');
+
+	});
+
+	//модалка на кнопку "получить консультацию"
+	$('.main_btn').click(function(){
+
+		$('.overlay').fadeToggle('slow');
+
+		$('.modal').slideToggle('slow');
+
+	});
+
+	//модалка на кнопку "расписание туров"
+	$('a[href*="#sheldure"]').click(function(){
 
 		$('.overlay').fadeToggle('slow');
 
