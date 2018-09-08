5e3b588793d6d264e91f89f9d88c3857c089b818
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-08 22:07:52 +0300

    изменения от 08.09.2018

--- final-exam/js/bundle/script.js
+++ final-exam/js/bundle/script.js
@@ -3,9 +3,10 @@
 window.addEventListener('DOMContentLoaded', function() {//проверка на то, что страница загрузилась т.е. скрипты начнут рабоать после загрузки страницы
 
 	let propEngineer = require('../parts/propEngineer.js'); //1) модальное окно prop_engineer
+	// let formMassage = require('../parts/formMassage.js'); //1) форма обратной связи
 
 	propEngineer();
-
+	// formMassage();
 
 
 });
