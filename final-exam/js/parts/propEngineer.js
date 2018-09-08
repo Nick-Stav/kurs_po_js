0b1c19e5e8de100d3ff9f4db65f5c217ee9e5173
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-08 17:16:36 +0300

    изменения на 08.09.2018

--- final-exam/js/parts/propEngineer.js
+++ final-exam/js/parts/propEngineer.js
@@ -1,6 +1,7 @@
 let headerBtn = document.getElementsByClassName('header_btn')[0],
 	popupEngineer = document.getElementsByClassName('popup_engineer')[0],
-	closeModalHead = document.getElementsByClassName('popup_close')[1];
+	closeModalHead = document.getElementsByClassName('popup_close')[1],
+	closeModalBack = document.getElementsByClassName('popup_dialog')[1];
 	
 
 
@@ -11,4 +12,6 @@ headerBtn.addEventListener('click', (event) => popupEngineer.style.display = 'fl
 closeModalHead.addEventListener('click', (event) => popupEngineer.style.display = 'none');
 
 //событие клик по полю экрана, закрывает модалку
-popupEngineer.addEventListener('click', (event) => popupEngineer.style.display = 'none');
+closeModalBack.addEventListener('click', (event) => popupEngineer.style.display = 'none');
+		
+	
