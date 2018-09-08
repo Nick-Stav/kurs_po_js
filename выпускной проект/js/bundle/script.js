08cd1b2ed17b57aad6d0b01c581d56644ead85d4
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-08 15:03:47 +0300

    от 08.09.2018

---
+++ выпускной проект/js/bundle/script.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+window.addEventListener('DOMContentLoaded', function() {//проверка на то, что страница загрузилась т.е. скрипты начнут рабоать после загрузки страницы
+
+	let propEngineer = require('../parts/propEngineer.js'); //1) модальное окно prop_engineer
+
+	propEngineer();
+
+
+
+});
