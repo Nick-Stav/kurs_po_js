17b577570d9bcd1ff33521b5e665014a867c7a3c
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-09 01:35:04 +0300

    изменения от 09.09.2018

--- final-exam/js/bundle/script.js
+++ final-exam/js/bundle/script.js
@@ -5,10 +5,14 @@ window.addEventListener('DOMContentLoaded', function() {//проверка на 
 	let propEngineer = require('../parts/propEngineer.js'); //1) модальное окно prop_engineer
 	// let formMassage = require('../parts/formMassage.js'); //2) форма обратной связи
 	let modalPopup = require('../parts/modalPopup.js'); //3) модальное окно prop_engineer
+	let timer = require('../parts/timer.js');//4) таймер
+
+
 
 	propEngineer();
 	// formMassage();
 	modalPopup();
+	timer();
 
 
 });
