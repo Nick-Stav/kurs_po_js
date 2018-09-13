226493e3b892682d8b5dcf4444d2830d9365626f
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-13 19:44:04 +0300

    1

--- final-exam/js/parts/timer.js
+++ final-exam/js/parts/timer.js
@@ -1,52 +1,53 @@
-let deadline = '2019/07/04';
+"use strict";
 
-function getTimeRemaining(endtime) {
-	let t = Date.parse(endtime) - Date.parse(new Date()),
-	seconds = Math.floor( (t/1000)%60 ), 
-	minutes = Math.floor((t/1000/60)%60),
-	hours = Math.floor( (t/(1000*60*60)) %24),
-	days = Math.floor( (t/(1000*60*60*24)) );
+function timer() {
+  var deadline = '2019/07/04';
+
+  function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor(t / 1000 % 60),
+        minutes = Math.floor(t / 1000 / 60 % 60),
+        hours = Math.floor(t / (1000 * 60 * 60) % 24),
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  } //функция, которая запускает часы
 
 
-	return {
-		'total': t,
-		'days' : days, 
-		'hours': hours,
-		'minutes': minutes,
-		'seconds': seconds
-	};
+  function setClock(id, endtime) {
+    var timer = document.getElementsByClassName('timer')[0],
+        days = timer.querySelector('.days'),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+      days.innerHTML = t.days < 10 ? '0' + t.days : t.days;
+      hours.innerHTML = t.hours < 10 ? '0' + t.hours : t.hours;
+      minutes.innerHTML = t.minutes < 10 ? '0' + t.minutes : t.minutes;
+      seconds.innerHTML = t.seconds < 10 ? '0' + t.seconds : t.seconds; //остановка таймера
+
+      if (t.total <= 0) {
+        var _timeInterval;
+
+        clearInterval(_timeInterval);
+        timer.innerHTML = '00:00:00';
+      }
+    }
+
+    updateClock();
+    var timeInterval = setInterval(updateClock, 1000);
+  } //конец функции setClock
+  //вызываем функцию запуска часов
+
+
+  setClock('timer', deadline);
 }
 
-//функция, которая запускает часы
-function setClock(id, endtime){
-
-	let timer = document.getElementsByClassName('timer')[0],
-		days = timer.querySelector('.days'),
-		hours = timer.querySelector('.hours'),
-		minutes = timer.querySelector('.minutes'),
-		seconds = timer.querySelector('.seconds');
-
-		function updateClock() {
-			let t = getTimeRemaining(endtime);
-			days.innerHTML = (t.days < 10) ? '0' + t.days : t.days;
-			hours.innerHTML = (t.hours < 10) ? '0' + t.hours : t.hours;
-			minutes.innerHTML = (t.minutes < 10) ? '0' + t.minutes : t.minutes;
-			seconds.innerHTML = (t.seconds < 10) ? '0' + t.seconds : t.seconds;
-
-			//остановка таймера
-			if (t.total <= 0) {
-				let timeInterval;
-				clearInterval(timeInterval);
-				timer.innerHTML = '00:00:00';
-			}
-		}
-
-		updateClock();
-		let timeInterval = setInterval(updateClock, 1000);
-
-		
-}//конец функции setClock
-
-//вызываем функцию щапуска часов
-setClock('timer', deadline);
-
+module.exports = timer;
