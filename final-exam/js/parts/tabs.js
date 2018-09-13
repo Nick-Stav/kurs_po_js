fcd37d160a71ac9f9e016edd02dea10910dc82f1
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-13 19:43:50 +0300

    1

--- final-exam/js/parts/tabs.js
+++ final-exam/js/parts/tabs.js
@@ -1,100 +1,90 @@
+"use strict";
+
 function tabs() {
+  var tabs = document.getElementsByClassName('glazing_slider')[0],
+      //список табов
+  tab = document.querySelectorAll('.tab'),
+      //сами табы
+  tabContent = document.getElementsByClassName('tab_content'); //содержание табов
+  //скрываем табы
 
-let tabs = document.getElementsByClassName('glazing_slider')[0],//список табов
-        tab = document.querySelectorAll('.tab'),//сами табы
-        tabContent = document.getElementsByClassName('tab_content');//содержание табов
-
-        //скрываем табы
-        function hideTabContent (a) {
-            for (let i = a; i < tabContent.length; i++) {
-                tabContent[i].classList.remove('show');
-                tabContent[i].classList.add('hide');
-                tab[i].classList.remove('active');
-
-            }
-        }
-
-		//оставляем активным первый таб
-        hideTabContent(1);
-
-        //показываем таб
-        function ShowTabContent (b) {
-            if (tabContent[b].classList.contains('hide')) {
-                hideTabContent(0);
-                tabContent[b].classList.remove('hide');
-                tabContent[b].classList.add('show');
-                tab[b].classList.add('active');
-
-            }
-        }
-
-
-        //событие
-        tabs.addEventListener('click', function(event) {
-            let target = event.target;
-            if(target.matches('.tab')) {
-                for (let i = 0; i < tab.length; i++) {
-                    if (target == tab[i]) {
-                        ShowTabContent(i);
-                        break;
-                    }
-                }
-            }
-
-        });
-
-
-
-
-// табы для отделки
-
-
-
-let tabsRem = document.querySelectorAll('.decoration_item'),//таб
-    tabAll = document.querySelector('.decoration_slider'), //все табы
-    tabTwoActive = document.querySelectorAll('.no_click'), //активный таб
-    tabContentTwo = document.querySelectorAll('.tabContentTwo'); //содержание табов
-
-
-    console.log(tabAll);
-    //скрываем табы
-     function hideTabContentTwo (a) {
-            for (let i = a; i < tabContentTwo.length; i++) {
-                tabContentTwo[i].classList.remove('show');
-                tabContentTwo[i].classList.add('hide');
-                tabTwoActive[i].classList.remove('after_click');
-
-            }
-        }
-
-        //оставляем активным первый таб
-        hideTabContentTwo(1);
-
-        //показываем таб
-        function ShowTabContentTwo (b) {
-            if (tabContentTwo[b].classList.contains('hide')) {
-                hideTabContentTwo(0);
-                tabContentTwo[b].classList.remove('hide');
-                tabContentTwo[b].classList.add('show');
-                tabTwoActive[b].classList.add('after_click');
-
-            }
-        }
-
-        //событие
-        tabAll.addEventListener('click', function(event) {
-            let target = event.target;
-            if(target.matches('.decoration_slider')) {
-                for (let i = 0; i < tabsRem.length; i++) {
-                    if (target == tabsRem[i]) {
-                        ShowTabContentTwo(i);
-                        break;
-                    }
-                }
-            }
-
-        });
-
+  function hideTabContent(a) {
+    for (var i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove('show');
+      tabContent[i].classList.add('hide');
+      tab[i].classList.remove('active');
     }
+  } //оставляем активным первый таб
+
+
+  hideTabContent(1); //показываем таб
+
+  function ShowTabContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+      hideTabContent(0);
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
+      tab[b].classList.add('active');
+    }
+  } //событие
+
+
+  tabs.addEventListener('click', function (event) {
+    var target = event.target;
+
+    if (target.matches('.tab')) {
+      for (var i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          ShowTabContent(i);
+          break;
+        }
+      }
+    }
+  }); // табы для отделки
+
+  var tabsRem = document.querySelectorAll('.decoration_item'),
+      //таб
+  tabAll = document.querySelector('.decoration_slider'),
+      //все табы
+  tabTwoActive = document.querySelectorAll('.no_click'),
+      //активный таб
+  tabContentTwo = document.querySelectorAll('.tabContentTwo'); //содержание табов
+
+  console.log(tabAll); //скрываем табы
+
+  function hideTabContentTwo(a) {
+    for (var i = a; i < tabContentTwo.length; i++) {
+      tabContentTwo[i].classList.remove('show');
+      tabContentTwo[i].classList.add('hide');
+      tabTwoActive[i].classList.remove('after_click');
+    }
+  } //оставляем активным первый таб
+
+
+  hideTabContentTwo(1); //показываем таб
+
+  function ShowTabContentTwo(b) {
+    if (tabContentTwo[b].classList.contains('hide')) {
+      hideTabContentTwo(0);
+      tabContentTwo[b].classList.remove('hide');
+      tabContentTwo[b].classList.add('show');
+      tabTwoActive[b].classList.add('after_click');
+    }
+  } //событие
+
+
+  tabAll.addEventListener('click', function (event) {
+    var target = event.target;
+
+    if (target.matches('.decoration_slider')) {
+      for (var i = 0; i < tabsRem.length; i++) {
+        if (target == tabsRem[i]) {
+          ShowTabContentTwo(i);
+          break;
+        }
+      }
+    }
+  });
+}
+
 module.exports = tabs;
- 
