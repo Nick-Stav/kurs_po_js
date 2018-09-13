8709d4b71d70263d1c5ccb28e7f0d273dc758d7d
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-13 19:43:30 +0300

    1

--- final-exam/js/parts/propEngineer.js
+++ final-exam/js/parts/propEngineer.js
@@ -1,17 +1,22 @@
-let headerBtn = document.getElementsByClassName('header_btn')[0],
-	popupEngineer = document.getElementsByClassName('popup_engineer')[0],
-	closeModalHead = document.getElementsByClassName('popup_close')[1],
-	closeModalBack = document.getElementsByClassName('popup_dialog')[1];
-	
+"use strict";
 
+function propEngineer() {
+  var headerBtn = document.getElementsByClassName('header_btn')[0],
+      popupEngineer = document.getElementsByClassName('popup_engineer')[0],
+      closeModalHead = document.getElementsByClassName('popup_close')[1],
+      closeModalBack = document.getElementsByClassName('popup_dialog')[1]; //событие клик по кнопке в голове, показывает модалку
 
-//событие клик по кнопке в голове, показывает модалку
-headerBtn.addEventListener('click', (event) => popupEngineer.style.display = 'flex');
+  headerBtn.addEventListener('click', function (event) {
+    return popupEngineer.style.display = 'flex';
+  }); //событие клик по кнопке в голове, закрывает модалку
 
-//событие клик по кнопке в голове, закрывает модалку
-closeModalHead.addEventListener('click', (event) => popupEngineer.style.display = 'none');
+  closeModalHead.addEventListener('click', function (event) {
+    return popupEngineer.style.display = 'none';
+  }); //событие клик по полю экрана, закрывает модалку
 
-//событие клик по полю экрана, закрывает модалку
-closeModalBack.addEventListener('click', (event) => popupEngineer.style.display = 'none');
-		
-	
+  closeModalBack.addEventListener('click', function (event) {
+    return popupEngineer.style.display = 'none';
+  });
+}
+
+module.exports = propEngineer;
