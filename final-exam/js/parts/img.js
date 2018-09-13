85e36201dfb8aa1972434a8e44552013e6f325f7
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-13 19:42:07 +0300

    1

--- final-exam/js/parts/img.js
+++ final-exam/js/parts/img.js
@@ -1,49 +1,52 @@
+"use strict";
+
 function img() {
-	let imageWorks = document.querySelector('.works'),//блок портфолио
-		link_image = imageWorks.getElementsByTagName('a'),//ссылки в блоке портфолио
-		image = imageWorks.getElementsByTagName('img'),//все картинки
-		divImage = document.createElement('div');//создаем новый блок
-		divImage.classList.add('div-image');//вешаем класс в див
-		document.body.appendChild(divImage);//выводим див
-		
+  var imageWorks = document.querySelector('.works'),
+      //блок портфолио
+  link_image = imageWorks.getElementsByTagName('a'),
+      //ссылки в блоке портфолио
+  image = imageWorks.getElementsByTagName('img'),
+      //все картинки
+  divImage = document.createElement('div'); //создаем новый блок
 
+  divImage.classList.add('div-image'); //вешаем класс в див
 
+  document.body.appendChild(divImage); //выводим див
+  //убираем лишний элемент - лупу
 
-		//убираем лишний элемент - лупу
-	for (let i = 0; i < image.length; i++){
-		if (image[i].classList.contains('lupa') == true) {
-			image[i].classList.add('image-gallery');
-			image[i].remove();
-		} else {
-			console.log('None');
-		}
-	}
+  for (var i = 0; i < image.length; i++) {
+    if (image[i].classList.contains('lupa') == true) {
+      image[i].classList.add('image-gallery');
+      image[i].remove();
+    } else {
+      console.log('None');
+    }
+  }
 
-	for (let i = 0; i < image.length; i++){
-		image[i].addEventListener('click', function(event){
-			event.preventDefault('link_image');//убираем дефолтное действие
-			
-			//работаем с адресом картинки
-			let index = i+1;
-			let imageNew = image[i].src = 'img/our_works/big_img/'+index+'.png';
-			image.src = 'img/our_works/'+index+'.png';
-			let imgQwe = divImage.getElementsByTagName('img');
-			divImage.innerHTML = '<div class="bigImg">' + '<img src="'+imageNew+'">' + '</div>';
+  var _loop = function _loop(_i) {
+    image[_i].addEventListener('click', function (event) {
+      event.preventDefault('link_image'); //убираем дефолтное действие
+      //работаем с адресом картинки
 
+      var index = _i + 1;
+      var imageNew = image[_i].src = 'img/our_works/big_img/' + index + '.png';
+      image.src = 'img/our_works/' + index + '.png';
+      var imgQwe = divImage.getElementsByTagName('img');
+      divImage.innerHTML = '<div class="bigImg">' + '<img src="' + imageNew + '">' + '</div>'; //запрещаем прокрутку
 
+      document.onmousewheel = document.onwheel = function () {
+        return false;
+      };
 
-			
-			//запрещаем прокрутку
-			document.onmousewheel=document.onwheel=function(){ 
-				return false;
-			};
-			document.addEventListener("MozMousePixelScroll",function(){return false;},false);
-			document.onkeydown=function(e) {
-				if (e.keyCode>=33&&e.keyCode<=40) return false;
-			};
+      document.addEventListener("MozMousePixelScroll", function () {
+        return false;
+      }, false);
 
-			
-			divImage.style.cssText="position: fixed; \
+      document.onkeydown = function (e) {
+        if (e.keyCode >= 33 && e.keyCode <= 40) return false;
+      };
+
+      divImage.style.cssText = "position: fixed; \
 			    overflow: hidden; \
 			    width: 100%; \
 			    height: 100%; \
@@ -52,30 +55,32 @@ function img() {
 			    text-align: center; \
 			    padding-bottom: 50px; \
 			  ";
+      image[_i].style.display = 'none'; //картинка в новом блоке
 
-			image[i].style.display = 'none';
+      divImage.addEventListener('click', function () {
+        image[_i].style.display = 'block';
+        divImage.style.display = 'none';
+        image[_i].src = 'img/our_works/' + index + '.png';
 
+        document.onmousewheel = document.onwheel = function () {
+          return true;
+        };
 
-			//картинка в новом блоке
-			divImage.addEventListener('click', function(){
-				image[i].style.display = 'block';
-				divImage.style.display = 'none';
-				image[i].src = 'img/our_works/'+index+'.png';
+        document.addEventListener("MozMousePixelScroll", function () {
+          return true;
+        }, true);
 
-				document.onmousewheel=document.onwheel=function(){ 
-					return true;
-				};
-				document.addEventListener("MozMousePixelScroll",function(){return true;},true);
-				document.onkeydown=function(e) {
-					if (e.keyCode>=33&&e.keyCode<=40) return true;
-				};
-			});
+        document.onkeydown = function (e) {
+          if (e.keyCode >= 33 && e.keyCode <= 40) return true;
+        };
+      });
+      event.preventDefault('link_image');
+    });
+  };
 
-
-
-			event.preventDefault('link_image');
-
-		});
-	}
+  for (var _i = 0; _i < image.length; _i++) {
+    _loop(_i);
+  }
 }
+
 module.exports = img;
