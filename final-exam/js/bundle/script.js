0a60479d8f9dc4402566274ee92a5e8adfe838bb
Nick-Stav <30655551+Nick-Stav@users.noreply.github.com>
2018-09-13 19:47:36 +0300

    1

--- final-exam/js/bundle/script.js
+++ final-exam/js/bundle/script.js
@@ -2,13 +2,13 @@
 
 window.addEventListener('DOMContentLoaded', function() {//проверка на то, что страница загрузилась т.е. скрипты начнут рабоать после загрузки страницы
 
-	let propEngineer = require('../parts/propEngineer.js'); //1) модальное окно prop_engineer
-	let formMassage = require('../parts/formMassage.js'); //2) форма обратной связи
-	let modalPopup = require('../parts/modalPopup.js'); //3) модальное окно prop_engineer
-	let timer = require('../parts/timer.js');//4) таймер
-	let tabs = require('../parts/tabs.js');//5) табы 
-	let img = require('../parts/img.js'); //6) картинки
-	let calc = require('../parts/calc.js'); //7) калькулятор
+	var propEngineer = require('../parts/propEngineer.js'); //1) модальное окно prop_engineer
+	var formMassage = require('../parts/formMassage.js'); //2) форма обратной связи
+	var modalPopup = require('../parts/modalPopup.js'); //3) модальное окно prop_engineer
+	var timer = require('../parts/timer.js');//4) таймер
+	var tabs = require('../parts/tabs.js');//5) табы 
+	var img = require('../parts/img.js'); //6) картинки
+	var calc = require('../parts/calc.js'); //7) калькулятор
 
 
 	propEngineer();
